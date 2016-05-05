e884c7242d091c34263dffc57225f5d83ab1f344
JP Richardson <jprichardson@gmail.com>
2016-05-04 22:28:46 -0500

    bitcoin-seed: add serialize() / fromBuffer()

--- src/index.js
+++ src/index.js
@@ -18,7 +18,33 @@ function create () {
     throw new Error('<bitcoin-seed> Error: Convert each Buffer to JSON individually.')
   }
 
+  bitcoinSeed.serialize = function serialize () {
+    var b = new Buffer(this.seed.length + this.mnemonic.length)
+    this.seed.copy(b)
+    this.mnemonic.copy(b, this.seed.length)
+    return b
+  }
+
   return bitcoinSeed
+}
+
+// NOTE: after calling this, don't forget to zero out the input buffer
+function fromBuffer (buffer) {
+  var bs = create()
+  bs.seed = new Buffer(64)
+  bs.mnemonic = new Buffer(buffer.length - 64)
+
+  buffer.copy(bs.seed, 0, 0, 64)
+  buffer.copy(bs.mnemonic, 0, 64, buffer.length)
+
+  return bs
+}
+
+function fromMnemonic (mnemonic, passphrase) {
+  var bs = create()
+  bs.mnemonic = new Buffer(mnemonic, 'utf8')
+  bs.seed = bip39.mnemonicToSeed(mnemonic, passphrase)
+  return bs
 }
 
 // options = { entropyFn, passphrase }
@@ -32,13 +58,6 @@ function fromRandom (options) {
   return fromMnemonic(mnemonic, options.passphrase)
 }
 
-function fromMnemonic (mnemonic, passphrase) {
-  var bs = create()
-  bs.mnemonic = new Buffer(mnemonic, 'utf8')
-  bs.seed = bip39.mnemonicToSeed(mnemonic, passphrase)
-  return bs
-}
-
 function isBitcoinSeed (bs) {
   return bs.mnemonic && bs.seed && ('isDestroyed' in bs)
 }
@@ -46,6 +65,7 @@ function isBitcoinSeed (bs) {
 module.exports = {
   create: create,
   isBitcoinSeed: isBitcoinSeed,
+  fromBuffer: fromBuffer,
   fromMnemonic: fromMnemonic,
   fromRandom: fromRandom
 }
--- test/bitcoin-seed.test.js
+++ test/bitcoin-seed.test.js
@@ -1,6 +1,11 @@
 var test = require('tape')
 var bitcoinSeed = require('../src')
 
+//  TODO: extract into fixtures file with more examples
+var FIXTURE_ENTROPY = 'c58d98f79da6e6ca93306d8c2fcc2bf6'
+var FIXTURE_SEED = '8ec7627de828cd686a14e8837919ed23a8bc52c6acaa719d3dfa37f1540dbd802038d8c1d12978ba69bb63d1297590db9015240deadbe48d96909e90e4dc0238'
+var FIXTURE_MNEMONIC = 'sheriff holiday digital deputy hover grab error asset method lazy april uniform'
+
 test('isBitcoinSeed() verify if object is a bitcoin seed (duck-type)', function (t) {
   t.plan(2)
 
@@ -14,11 +19,11 @@ test('isBitcoinSeed() verify if object is a bitcoin seed (duck-type)', function 
 test('fromRandom() generates a random seed and mnemonic', function (t) {
   t.plan(3)
 
-  var entropyFn = function () { return new Buffer('c58d98f79da6e6ca93306d8c2fcc2bf6', 'hex') }
+  var entropyFn = function () { return new Buffer(FIXTURE_ENTROPY, 'hex') }
   var bs = bitcoinSeed.fromRandom({ entropyFn: entropyFn })
 
-  t.is(bs.seed.toString('hex'), '8ec7627de828cd686a14e8837919ed23a8bc52c6acaa719d3dfa37f1540dbd802038d8c1d12978ba69bb63d1297590db9015240deadbe48d96909e90e4dc0238', 'seed')
-  t.is(bs.mnemonic.toString('utf8'), 'sheriff holiday digital deputy hover grab error asset method lazy april uniform', 'mnemonic')
+  t.is(bs.seed.toString('hex'), FIXTURE_SEED, 'seed')
+  t.is(bs.mnemonic.toString('utf8'), FIXTURE_MNEMONIC, 'mnemonic')
   t.false(bs.isDestroyed, 'destroyed = false')
 
   t.end()
@@ -27,7 +32,7 @@ test('fromRandom() generates a random seed and mnemonic', function (t) {
 test('destroy() fills buffers with zero', function (t) {
   t.plan(3)
 
-  var entropyFn = function () { return new Buffer('c58d98f79da6e6ca93306d8c2fcc2bf6', 'hex') }
+  var entropyFn = function () { return new Buffer(FIXTURE_ENTROPY, 'hex') }
   var bs = bitcoinSeed.fromRandom({ entropyFn: entropyFn })
 
   bs.destroy()
@@ -35,6 +40,22 @@ test('destroy() fills buffers with zero', function (t) {
   t.is(bs.seed.toString('hex'), '00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000', 'seed is zero')
   t.is(bs.mnemonic.toString('hex'), '00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000', 'mnemonic is zero')
   t.true(bs.isDestroyed, 'destroyed = true')
+
+  t.end()
+})
+
+test('serialize() / fromBuffer() should serialize to buffer and deserialize from buffer', function (t) {
+  t.plan(3)
+
+  var entropyFn = function () { return new Buffer(FIXTURE_ENTROPY, 'hex') }
+  var bs = bitcoinSeed.fromRandom({ entropyFn: entropyFn })
+
+  var buf = bs.serialize()
+  t.is(buf.toString('hex'), FIXTURE_SEED + new Buffer(FIXTURE_MNEMONIC).toString('hex'), 'SEED + MNEMONIC')
+
+  var bs2 = bitcoinSeed.fromBuffer(buf)
+  t.is(bs2.seed.toString('hex'), FIXTURE_SEED, 'seed')
+  t.is(bs2.mnemonic.toString('utf8'), FIXTURE_MNEMONIC, 'mnemonic')
 
   t.end()
 })

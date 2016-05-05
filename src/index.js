6d071954c5b80e5bad95dc214a1f2f53ffbbb8a8
JP Richardson <jprichardson@gmail.com>
2016-05-04 22:40:25 -0500

    src/index: more comments

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ function create () {
     throw new Error('<bitcoin-seed> Error: Convert each Buffer to JSON individually.')
   }
 
+  // => seed || mnemonic
   bitcoinSeed.serialize = function serialize () {
     var b = new Buffer(this.seed.length + this.mnemonic.length)
     this.seed.copy(b)
@@ -29,6 +30,9 @@ function create () {
 }
 
 // NOTE: after calling this, don't forget to zero out the input buffer
+// could use slice() here, but in Browserify Buffer, that may be deceiving in
+// older browsers: https://github.com/feross/buffer#in-old-browsers-bufslice-does-not-modify-parent-buffers-memory
+// may be an acceptable tradeoff at some point
 function fromBuffer (buffer) {
   var bs = create()
   bs.seed = new Buffer(64)
